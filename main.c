cda5253f76d70351ef9501c07abcbe58a618d194
Daniel <pv28506@alunos.estgv.ipv.pt>
2024-04-23 10:15:28 +0100

    Main menu

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include "src/Hashing.h"
 #include "src/Biblioteca.h"
 
-//olaaaaaaaaa
+
 
 extern int LerInteiro(char *txt);
 //-----------------------------------------
-void Exemplo_Hashing()
+/*void Exemplo_Hashing()
 {
     HASHING *Has = CriarHashing();
     PESSOA *X = CriarPessoa(1234, "Jose", "CAT-A");
@@ -25,40 +25,74 @@ void Exemplo_Hashing()
     ShowHashing(Has);
 
     DestruirHashing(Has);
-}
+}*/
 int Menu()
 {
-    printf("1- Load\n");
-    printf("2- Show\n");
-    printf("3- Nome mais Comum\n");
-    printf("0-Sair\n");
+    printf("1- Adicionar Livro\n");
+    printf("2- Listar Livros\n");
+    printf("3- Pesquisar Livro por ISBN\n");
+    printf("4- Encontrar Livro Mais Recente\n");
+    printf("0- Sair\n");
     int op;
-    op = LerInteiro("Qual a o opcao? ");
+    op = LerInteiro("Qual a opcao? ");
     return op;
 }
 
 int main()
 {
-    printf("Está a executar sem erros!");
-    /*printf("Projecto-Biblioteca-Versao-Base!\n");
+    printf("Projecto-Biblioteca-Versao-Base!\n");
     //Exemplo_Hashing();
-    BIBLIOTECA *Bib;
-    Bib = CriarBiblioteca("Biblioteca-ESTGV", "../data/log.txt");
+    ListaLivro *listaLivros = criarLista(); // Criar a lista de livros
     int OP;
     do
     {
         OP = Menu();
         switch(OP)
         {
-            case 1: LoadFicheiroBiblioteca(Bib); break;
-            case 2: ShowBiblioteca(Bib); break;
+            case 1:
+                MostrarLivro(PedirDadosLivro());
+                break;
+            case 2: {
+                ListarLivros(listaLivros);
+                break;
+            }
+            case 3: {
+                char ISBN [20];
+                printf("Digite o ISBN do livro a pesquisar: ");
+                scanf("%19s", ISBN);
+                LIVRO *livroEncontrado = PesquisarLivroPorISBN((LIVRO *) listaLivros->Inicio, listaLivros->num_Livros, ISBN);
+                if (livroEncontrado != NULL) {
+                    MostrarLivro(livroEncontrado);
+                } else {
+                    printf("Livro com ISBN %s nao encontrado.\n", ISBN);
+                }
+                break;
+            }
+            case 4: {
+                LIVRO *livroMaisRecente = LivroMaisRecente(listaLivros);
+                if (livroMaisRecente != NULL) {
+                    printf("Livro mais recente:\n");
+                    MostrarLivro(livroMaisRecente);
+                } else {
+                    printf("Nenhum livro disponível.\n");
+                }
+                break;
+            }
             default:
-                printf("Opcao nao implementada\n"); break;
+                printf("Opção não implementada\n");
+                break;
         }
 
-    }while (OP != 0);
-    DestruirBiblioteca(Bib);
-    return EXIT_SUCCESS; // ou EXIT_FAILURE
-     */
+    } while (OP != 0);
+    // Liberar memória alocada para a lista de livros
+    Elemento *e = listaLivros->Inicio;
+    while (e != NULL) {
+        Elemento *prox = e->proximo;
+        DestruirLivro(e->livro);
+        free(e);
+        e = prox;
+    }
+    free(listaLivros);
 
+    return EXIT_SUCCESS; // ou EXIT_FAILURE
 }
