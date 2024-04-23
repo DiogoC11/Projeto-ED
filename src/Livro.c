37842e6ca2d6267d8ea243795600fe622d9eae12
DiogoC11 <diogo.a.cardoso.2005@gmail.com>
2024-04-23 09:58:15 +0100

    alteracao da funcao criar livro 2

--- src/Livro.c
+++ src/Livro.c
@@ -1,7 +1,7 @@
 
 #include "Livro.h"
 
-LIVRO *CriarLivro( char *_ISBN, char *_nome, char *_area, int _anoPublicacao)
+LIVRO *CriarLivro( char *_ISBN, char *_nome, char *_area, int _anoPublicacao, char *_autor)
 {
     LIVRO *P = (LIVRO *)malloc(sizeof(LIVRO));
     P->NOME = (char *)malloc((strlen(_nome) + 1)*sizeof(char));
@@ -11,11 +11,12 @@ LIVRO *CriarLivro( char *_ISBN, char *_nome, char *_area, int _anoPublicacao)
     P->anoPublicacao = _anoPublicacao;
     P->ISBN = (char *) malloc(strlen(_ISBN + 1)*sizeof(char));
     P->ISBN = _ISBN;
-    //P->ID = _id;
+    P->Autor = (char *) malloc(strlen(_autor + 1)*sizeof(char));
+    P->Autor = _autor;
     return P;
 }
 LIVRO *PedirDadosLivro(){
-    char nome[50],area[50],ISBN[50];
+    char nome[50],area[50],ISBN[50],autor[50];
     int anoPublicacao;
     printf("\nCriar Livro: ");
     printf("\nTitulo do Livro: ");
@@ -26,11 +27,13 @@ LIVRO *PedirDadosLivro(){
     scanf("%d",anoPublicacao);
     printf("\nISBN: ");
     scanf("%s",ISBN);
-    return CriarLivro(ISBN,nome,area,anoPublicacao);
+    printf("\nAutor do Livro: ");
+    scanf("%s",autor);
+    return CriarLivro(ISBN,nome,area,anoPublicacao,autor);
 }
 void MostrarLivro(LIVRO *P)
 {
-    printf("\tPESSOA: ID: [%s] [%s] [%s]\n", P->ISBN, P->NOME, P->AREA);
+    printf("\tLivro: \nISBN: [%s] \nTitulo: [%s] \nArea: [%s] \nAutor: [%s] \nAno de Publicacao: [%d]\n", P->ISBN, P->NOME, P->AREA, P->Autor, P->anoPublicacao);
 }
 void DestruirLivro(LIVRO *P)
 {
