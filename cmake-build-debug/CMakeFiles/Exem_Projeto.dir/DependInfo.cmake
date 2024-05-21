e261aea82968268501ee39668cf380a418f65527
admin <pdiogo991@gmail.com>
2024-05-21 12:37:37 +0100

    teste

---
+++ cmake-build-debug/CMakeFiles/Exem_Projeto.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/admin/Desktop/Projeto-ED/main.c" "CMakeFiles/Exem_Projeto.dir/main.c.obj" "gcc" "CMakeFiles/Exem_Projeto.dir/main.c.obj.d"
+  "C:/Users/admin/Desktop/Projeto-ED/src/Biblioteca.c" "CMakeFiles/Exem_Projeto.dir/src/Biblioteca.c.obj" "gcc" "CMakeFiles/Exem_Projeto.dir/src/Biblioteca.c.obj.d"
+  "C:/Users/admin/Desktop/Projeto-ED/src/Livro.c" "CMakeFiles/Exem_Projeto.dir/src/Livro.c.obj" "gcc" "CMakeFiles/Exem_Projeto.dir/src/Livro.c.obj.d"
+  "C:/Users/admin/Desktop/Projeto-ED/src/Pessoa.c" "CMakeFiles/Exem_Projeto.dir/src/Pessoa.c.obj" "gcc" "CMakeFiles/Exem_Projeto.dir/src/Pessoa.c.obj.d"
+  "C:/Users/admin/Desktop/Projeto-ED/src/Requisicao.c" "CMakeFiles/Exem_Projeto.dir/src/Requisicao.c.obj" "gcc" "CMakeFiles/Exem_Projeto.dir/src/Requisicao.c.obj.d"
+  "C:/Users/admin/Desktop/Projeto-ED/src/Uteis.c" "CMakeFiles/Exem_Projeto.dir/src/Uteis.c.obj" "gcc" "CMakeFiles/Exem_Projeto.dir/src/Uteis.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
